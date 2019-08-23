bd7b99cebddbf06c4fd2c8bae270fc0dc65b6bd0
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-23 11:40:24 -0500

    is it charecter or character???
    I CANT SPELL going with character tho

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import CharecterCard from './CharacterCard'
+import CharacterCard from './CharacterCard'
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const[charecters, setCharecters]=useState([]);
+  const[characters, setCharacters]=useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
@@ -18,7 +18,19 @@ export default function CharacterList() {
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+  {
+        characters.map(char => (
+          <CharacterCard
+          key={char.id}
+          name={char.name}
+          status={char.status}
+          species={char.species}
+          origin={char.origin.name}
+          location={char.location.name}
+          image={char.image}
+          />
+        ))
+      }
     </section>
   );
-
+}
