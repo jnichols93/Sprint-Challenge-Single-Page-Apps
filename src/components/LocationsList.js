c3c4ba96402f04f37cc4baf077680e6f7931ecd6
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-23 11:45:39 -0500

    Standort Liste ist FERTIG
    Haben sie Ich der Standort Liste gemacht, es war SEHR schwierig an meinen Fingern. Aber es ist fertig SOOOOO JA.

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,31 @@
 import React, { useEffect, useState } from "react";
+import LocationCard from './LocationCard';
+import axios from 'axios';
 
-export default function LocationsList() {}
+export default function LocationsList() {
+const [locations, setLocations] = useState([]);
+
+useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location/')
+        .then(dataset => {
+            // console.log(dataset.data.results);
+            setLocations(dataset.data.results)
+        }).catch(err => {
+            console.log('Something went wrong with the list', err);
+        })
+}, [])
+
+return (
+    <section className="grid-view">
+        {locations.map(location => (
+            <LocationCard
+                key={location.id}
+                name={location.name}
+                type={location.type}
+                dimension={location.dimension}
+                residents={location.residents}
+            />
+        ))}
+    </section>
+)
+}
