069b5836e5913e6d3a551353c18c8cff036b03be
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-23 10:35:36 -0500

    Update EpisodeCard.js
    finsished the card for episodes

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -4,3 +4,26 @@ import EpisodeCard from './EpisodeCard';
 
 export default function EpisodeList() {
     const [episodes, setEpisodes ]= useState([]);
+
+    useEffect(() => {
+        Axios.get('https://rickandmortyapi.com/api/episode/')
+        .then(dataset => {
+            // console.log(dataset.data);
+            setEpisodes(dataset.data.results)
+        }).catch(err => {
+            console.log('Something went wrong in Epsiodes: ', error)
+        })
+    }, [])
+    return (
+        <section className="grid-view">
+            {episodes.map(episode => (
+                <EpisodeCard 
+                key={episode.id}
+                name={episode.name}
+                airDate={episode.air_date}
+                episode={episode.episode}
+                />
+            ))}
+        </section>
+    )
+}
