e5eb479d03a232e821bb30656387747cd8e067e4
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-23 11:37:17 -0500

    changed welcome to wubbablubadubdub
    also created a seperate router component for a more zen like coding environment

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -4,7 +4,7 @@ export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
-        <h1>Welcome to the ultimate fan site!</h1>
+        <h1>WUBBALUBBA BUB DUB!</h1>
         <img
           className="main-img ui centered medium circular image"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
